2de73dbb3939604e2f18ac9c092e7077d235bad4
DeveloperAmrit <kittyamrit005@gmail.com>
2024-12-26 18:28:10 +0530

    new file:   .gitignore 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/App.test.js 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/reportWebVitals.js 	new file:   src/setupTests.js 	new file:   tailwind.config.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-screen text-3xl flex justify-center items-center">
+      Hello This is going to be out squadron's website
+    </div>
+  );
+}
+
+export default App;

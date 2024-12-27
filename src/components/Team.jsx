71695f95edbef0b8e22c2ee7d4f43a97bf4f873f
DeveloperAmrit <kittyamrit005@gmail.com>
2024-12-27 16:19:38 +0530

    modified:   public/favicon.ico 	modified:   public/index.html 	modified:   src/components/Team.jsx 	modified:   src/components/WorkDone.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -6,7 +6,7 @@ import { VP, OC, coordinators, members } from '../data/decaStrikeTeam';
 const Logo = () => {
   return (
     <div className='flex justify-center items-center transition-transform duration-300 hover:scale-110'>
-      <img className='w-56 h-56 rounded-full' src="/assets/images/decastrike-logo.jpg" alt="LION LOGO" />
+      <img className='w-36 h-36 md:w-40 md:h-40 lg:w-56 lg:h-56 rounded-full' src="/assets/images/decastrike-logo.jpg" alt="LION LOGO" />
     </div>
   );
 }
@@ -15,7 +15,7 @@ export const Seperator = ({ title }) => {
   return (
     <div className='w-full flex justify-center items-center py-10 gap-3'>
       <div className='w-52 bg-gray-500 h-[2px]'></div>
-      <div className='text-2xl font-bold'>{title}</div>
+      <div className='text-lg sm:text-2xl font-bold'>{title}</div>
       <div className='w-52 bg-gray-500 h-[2px]'></div>
     </div>
   );
@@ -24,14 +24,16 @@ export const Seperator = ({ title }) => {
 
 const Team = () => {
   return (
-    <div className='w-full flex flex-col justify-centerv items-center'>
-      <div className='w-full flex justify-center gap-10 mt-6'>
+    <div className='w-full flex flex-col justify-centerv items-center whitespace-nowrap'>
+      <div className='w-full flex flex-col sm:flex-row justify-center gap-10 mt-6'>
         <Logo />
         <div className='flex flex-col justify-center items-center'>
           <h1 className='py-2 text-4xl font-extrabold tracking-tight text-gray-900 transition-transform duration-300 hover:text-gray-950'>{name}</h1>
           <p className='text-lg'>{tagline}</p>
         </div>
-        <Logo />
+        <div className='hidden sm:block'>
+          <Logo />
+        </div>
       </div>
       <Seperator title="VICE PRESIDENT" />
       <Card
@@ -57,7 +59,7 @@ const Team = () => {
         })}
       </div>
       <Seperator title="SQUADRON LEADERS" />
-      <div className='w-full grid grid-flow-row grid-cols-3'>
+      <div className='w-full grid grid-flow-row grid-cols-1 gap-y-3 md:grid-cols-2 lg:grid-cols-3'>
         {coordinators.map((element) => {
           return (
             <Card
@@ -73,7 +75,7 @@ const Team = () => {
         })}
       </div>
       <Seperator title="SPARTANS" />
-      <div className='w-full grid grid-flow-row grid-cols-3'>
+      <div className='w-full grid grid-flow-row grid-cols-1 gap-y-3  md:grid-cols-2 lg:grid-cols-3'>
         {members.map((element) => {
           return (
             <Card
--- src/components/WorkDone.jsx
+++ src/components/WorkDone.jsx
@@ -4,15 +4,15 @@ import { achievements } from '../data/decaStrikeAcheivements';
 
 const WorkCard = ({imgsrc,title,description,ytlink,insta,date}) => {
     return (
-        <div className='w-96 pb-3 flex flex-col items-center border-2 border-gray-400 rounded-sm'>
+        <div className='w-full max-w-96 pb-3 flex flex-col items-center border-2 border-gray-400 rounded-sm'>
             <div className='w-full'>
                 <img className='w-full h-72 transition-transform duration-300 hover:scale-110' src={imgsrc} alt="MEET PIC" />
             </div>
-            <h1 className='text-2xl py-3 font-semibold p-3'>{title}</h1>
+            <h1 className='text-xl sm:text-2xl py-3 font-semibold p-3'>{title}</h1>
             <p className='p-3 '>{description}</p>
             <div className='w-full py-3 flex justify-evenly items-center'>
-                {ytlink && <a className='px-2 py-3 bg-rose-500 text-white rounded-sm hover:bg-rose-600 active:bg-rose-800' href={ytlink} target='__blank'>Full Interview</a>}
-                {insta && <a className='px-2 py-3 bg-pink-500 text-white rounded-sm hover:bg-pink-600 active:bg-pink-800' href={insta} target='__blank'>Instagram Post</a>}
+                {ytlink && <a className='px-2 py-3 bg-rose-500 text-white rounded-sm hover:bg-rose-600 active:bg-rose-800 transition-transform duration-300 hover:scale-110' href={ytlink} target='__blank'>Full Interview</a>}
+                {insta && <a className='px-2 py-3 bg-pink-500 text-white rounded-sm hover:bg-pink-600 active:bg-pink-800 transition-transform duration-300 hover:scale-110' href={insta} target='__blank'>Instagram Post</a>}
             </div>
             <p className='p-3'>{date}</p>
         </div>
@@ -21,9 +21,9 @@ const WorkCard = ({imgsrc,title,description,ytlink,insta,date}) => {
 
 const WorkDone = () => {
     return (
-        <div>
+        <div className='flex flex-col items-center'>
             <Seperator title="ACHIEVEMENTS" />
-            <div className='w-full grid grid-flow-row grid-cols-3'>
+            <div className='md:w-full grid grid-flow-row grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3'>
                 {achievements.map((element)=>{
                     return <WorkCard key={element.imgsrc} imgsrc={element.imgsrc} title={element.title} description={element.description} ytlink={element.ytlink} insta={element.insta} date={element.date}/>
                 })}
